7f3189bf2ce4725c46a2355814a48c66001ef1ba
BrunoPrdt <bruno.predot@gmail.com>
2020-07-29 10:53:34 +0200

    add change

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-
+    <TheHeader/>
   </div>
 </template>
 
 <script>
+  import TheHeader from "./components/TheHeader/TheHeader";
 
 export default {
   name: 'App',
   components: {
-
+    TheHeader
   }
 }
 </script>
 
 <style>
+#app {
+}
 </style>
