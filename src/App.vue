6bdcc7634b4965c979c839b8c4a91dfc0e23dfb7
BrunoPrdt <bruno.predot@gmail.com>
2020-07-31 08:49:13 +0200

    change components declaration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
+  <div  id="app">
     <TheHeader/>
-    <div class="d-flex flex-row">
-      <Shop class="w-75"></Shop>
-      <Cart class="w-25" :appInfos="appInfos"></Cart>
-    </div>
   </div>
 </template>
 
 <script>
   import TheHeader from "./components/TheHeader/TheHeader";
-  import Shop from "./components/features/User/Shop/Shop/Shop";
-  import Cart from "./components/features/User/Cart/Cart/Cart";
 
 export default {
   name: 'App',
   components: {
     TheHeader,
-    Shop,
-    Cart,
   },
   data(){
     return{
-      appInfos: "Je viens de ap (data bind)",
     }
   },
 }
---
+++ src/components/features/User/User/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="d-flex flex-row">
+            <Shop class="w-75"></Shop>
+            <Cart class="w-25"></Cart>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Shop from "../Shop/Shop/Shop";
+    import Cart from "../Cart/Cart/Cart";
+
+    export default {
+        components: {
+            Shop,
+            Cart,
+        },
+        props: {
+        },
+    };
+</script>
+
+<style></style>
